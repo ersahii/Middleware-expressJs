159c3100cba01d7cf89f77d79e813d17309ab48d
mirmehraj <mehraj95he@gmail.com>
2024-06-12 15:33:36 +0530

    Created Band Name generator

--- index4.js
+++ index4.js
@@ -1,14 +1,17 @@
 import express from "express";
+const app = express();
 import bodyParser from "body-parser";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
 const _dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }))
-const app = express();
 app.listen(port , (req , res)=>{
     console.log("server started! at port" , port);
 })
 app.get("/", (req , res)=>{
     res.sendFile(_dirname + "/public/home2.html")
+    })
+app.post("/submit", (req , res)=>{
+    res.send(`<h1>Your Band Name is :</h1>${req.body.streetName}${req.body.petName}`);
 })
